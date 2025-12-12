861e290409200709ba9f9feb73958480c464f897
Moises Sandoval <moises.sandoval.rivero@gmail.com>
2025-12-12 17:20:08 -0600

    Finalizacion del trabajo

--- src/gui/acercaDePanel.java
+++ src/gui/acercaDePanel.java
@@ -2,6 +2,10 @@ package gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 import javax.swing.*;
 
 public class acercaDePanel extends JPanel {
@@ -25,7 +29,74 @@ private static final String[] integrantes = {
         panelAcercaDe.add(new JLabel("Curso: 5CM1 - Redes de Computadoras"));
         panelAcercaDe.add(new JLabel("Profesor: Alcaraz Torres Juan Jesus"));
 
-        add(panelAcercaDe); 
+        JButton btnGitHub = new JButton("Repositorio GitHub"); 
+        btnGitHub.setPreferredSize(new Dimension(150, 40));
+        btnGitHub.setBackground(new Color(50, 50, 50)); 
+        btnGitHub.setForeground(Color.WHITE);
+
+        btnGitHub.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                abrirEnlace("https://github.com/Mois3sSE/calculadoraDeRedes_ESCOM");
+            }
+        }); 
+                  
+        JButton bntReadMe = new JButton("Ver ReadMe");
+        bntReadMe.setPreferredSize(new Dimension(150, 40));
+        bntReadMe.setBackground(new Color(237, 201, 194));
+        bntReadMe.setForeground(Color.WHITE);
+        bntReadMe.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                abrirReadMe("README.md");
+            }
+        });
+
+        JPanel panelBoton = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 10));
+        panelBoton.add(btnGitHub);
+        panelBoton.add(new JLabel("             ")); 
+        panelBoton.add(bntReadMe);
+
+        panelAcercaDe.add(new JLabel("Derechos reservados © 2025 - ESCOM - IPN"));
+
+        panelAcercaDe.add(panelBoton);
+        add(panelAcercaDe,BorderLayout.NORTH); 
+    }
+
+    // Metodos auxiliares 
+
+    private void abrirEnlace(String url) {
+        try {
+            if(Desktop.isDesktopSupported()){
+            Desktop desktop =  Desktop.getDesktop();
+                if(desktop.isSupported(Desktop.Action.BROWSE)){
+                    desktop.browse(new java.net.URI(url));
+                }
+            }
+        } catch (IOException | URISyntaxException ex) {
+           JOptionPane.showMessageDialog(this, "No se pudo abrir el navegador: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    private void abrirReadMe(String nombreArchivo) {
+        try {
+            
+            File archivo = new File(nombreArchivo);
+
+            if (archivo.exists()) {
+                if (Desktop.isDesktopSupported()) {
+                    Desktop.getDesktop().open(archivo);
+                } else {
+                    JOptionPane.showMessageDialog(this, "El sistema no soporta abrir archivos automáticamente.");
+                }
+            } else {
+                JOptionPane.showMessageDialog(this, 
+                    "No se encontró el archivo: " + nombreArchivo + "\n" +
+                    "Ruta buscada: " + archivo.getAbsolutePath(), 
+                    "Archivo no encontrado", JOptionPane.WARNING_MESSAGE);
+            }
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(this, "Error al abrir el archivo: " + ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 
 }
